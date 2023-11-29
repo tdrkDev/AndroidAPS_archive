520a0b41ba6d2105289bdc9178b742f4a3bde737
ga-zelle <ga.zelle@netcologne.de>
2023-11-30 00:50:44 +0100

    reactivate tdd to generate screenshot for Milos

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSSMB/OpenAPSSMBPlugin.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSSMB/OpenAPSSMBPlugin.kt
@@ -240,22 +240,21 @@ open class OpenAPSSMBPlugin @Inject constructor(
         // without these values DynISF doesn't work properly
         // Current implementation is fallback to SMB if TDD history is not available. Thus calculated here
 
-        //tdd1D = tddCalculator.averageTDD(tddCalculator.calculate(1, allowMissingDays = false))?.totalAmount
-        //tdd7D = tddCalculator.averageTDD(tddCalculator.calculate(7, allowMissingDays = false))?.totalAmount
-        //tddLast24H = tddCalculator.calculateDaily(-24, 0)?.totalAmount
-        //tddLast4H = tddCalculator.calculateDaily(-4, 0)?.totalAmount
-        //tddLast8to4H = tddCalculator.calculateDaily(-8, -4)?.totalAmount
-//
-        //if (tdd1D == null || tdd7D == null || tddLast4H == null || tddLast8to4H == null || tddLast24H == null) {
-        //    inputConstraints.copyReasons(
-        //        ConstraintObject(false, aapsLogger).also {
-        //            it.set(false, rh.gs(R.string.fallback_smb_no_tdd), this)
-        //        }
-        //    )
-        //    inputConstraints.copyReasons(
-        //        ConstraintObject(false, aapsLogger).apply { set(true, "tdd1D=$tdd1D tdd7D=$tdd7D tddLast4H=$tddLast4H tddLast8to4H=$tddLast8to4H tddLast24H=$tddLast24H", this) }
-        //    )
-        //}
+        tdd1D = tddCalculator.averageTDD(tddCalculator.calculate(1, allowMissingDays = false))?.totalAmount
+        tdd7D = tddCalculator.averageTDD(tddCalculator.calculate(7, allowMissingDays = false))?.totalAmount
+        tddLast24H = tddCalculator.calculateDaily(-24, 0)?.totalAmount
+        tddLast4H = tddCalculator.calculateDaily(-4, 0)?.totalAmount
+        tddLast8to4H = tddCalculator.calculateDaily(-8, -4)?.totalAmount
+        if (tdd1D == null || tdd7D == null || tddLast4H == null || tddLast8to4H == null || tddLast24H == null) {
+            inputConstraints.copyReasons(
+                ConstraintObject(false, aapsLogger).also {
+                    it.set(false, rh.gs(R.string.fallback_smb_no_tdd), this)
+                }
+            )
+            inputConstraints.copyReasons(
+                ConstraintObject(false, aapsLogger).apply { set(true, "tdd1D=$tdd1D tdd7D=$tdd7D tddLast4H=$tddLast4H tddLast8to4H=$tddLast8to4H tddLast24H=$tddLast24H", this) }
+            )
+        }
 
 
         provideDetermineBasalAdapter().also { determineBasalAdapterSMBJS ->
