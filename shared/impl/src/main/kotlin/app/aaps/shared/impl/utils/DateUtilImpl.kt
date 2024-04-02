cfc7ff462a847e6cfff8309e7ba1438b931c7b9d
ga-zelle <ga.zelle@netcologne.de>
2024-04-02 23:31:47 +0200

    show sec ago for last BG

--- shared/impl/src/main/kotlin/app/aaps/shared/impl/utils/DateUtilImpl.kt
+++ shared/impl/src/main/kotlin/app/aaps/shared/impl/utils/DateUtilImpl.kt
@@ -218,6 +218,16 @@ class DateUtilImpl @Inject constructor(private val context: Context) : DateUtil 
         val minutes = ((now() - time) / 1000 / 60).toInt()
         return rh.gs(R.string.minago, minutes)
     }
+    override fun minOrSecAgo(rh: ResourceHelper, time: Long?): String {
+        if (time == null) return ""
+        //val minutes = ((now() - time) / 1000 / 60).toInt()
+        val seconds = (now() - time) / 1000
+        if (seconds > 99) {
+            return rh.gs(R.string.minago, (seconds / 60).toInt())
+        } else {
+            return rh.gs(R.string.secago, seconds.toInt())
+        }
+    }
 
     override fun minAgoShort(time: Long?): String {
         if (time == null) return ""
